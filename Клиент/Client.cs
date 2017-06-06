499d4ab6883e7c49ebc35df1e6e5245b9e32ec44
Reestor <pavel_g97@mail.ru>
2017-06-07 00:33:55 +0300

    Add files via upload

---
+++ Клиент/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using ZeroMQ;
+
+namespace Client1
+{
+    static partial class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            if (args == null || args.Length < 1)
+            {
+                Console.WriteLine("Клиент Гжелин П.В.:\n");
+                args = new string[] { "tcp://192.168.1.33:8005" };
+            }
+
+            using (var context = new ZContext())
+            using (var subscriber = new ZSocket(context, ZSocketType.SUB))
+            {
+                subscriber.Connect(args[0]);
+
+                var rnd = new Random();
+                var subscription = string.Format("{0:D3}", rnd.Next(10));
+                subscriber.Subscribe(subscription);
+
+                ZMessage msg;
+                ZError error;
+                while (true)
+                {
+                    if (null == (msg = subscriber.ReceiveMessage(out error)))
+                    {
+                        if (error == ZError.ETERM)
+                            break;  
+                        throw new ZException(error);
+                    }
+                    using (msg)
+                    {
+                        if (msg[0].ReadString() != subscription)
+                        {
+                            throw new InvalidOperationException();
+                        }
+                        Console.WriteLine(msg[1].ReadString());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Сервер/Server.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using ZeroMQ;
+
+namespace Server
+{
+    static partial class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            if (args == null || args.Length < 1)
+            {
+                Console.WriteLine("Сервер\n");
+                args = new string[] { null };
+            }
+
+            using (var context = new ZContext())
+            using (var publisher = new ZSocket(context, ZSocketType.PUB))
+            {
+                if (args[0] != null)
+                {
+                    publisher.Connect(args[0]);
+                }
+                else
+                {
+                    publisher.Bind("tcp://192.168.1.33:8005");
+                }
+
+                var rnd = new Random();
+
+                while (true)
+                {
+                    publisher.SendMore(new ZFrame(string.Format("{0:D3}", rnd.Next(10))));
+                    publisher.Send(new ZFrame("Здравствуйте клиент!"));
+                    Console.WriteLine("Отправка сообщения");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
